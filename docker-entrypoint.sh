17e80bdf67a12a712d0b48f0c75e6dc397f071ca
Alban Montaigu <alban.montaigu@gmail.com>
2015-10-13 12:18:47 +0200

    Entrypoint command now give more information

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,9 +11,9 @@ tar -zcf /var/backup/shaarli/shaarli-v$(date '+%Y%m%d%H%M%S').tar.gz .
 # File copy strategy taken from wordpress entrypoint
 # @see https://github.com/docker-library/wordpress/blob/master/fpm/docker-entrypoint.sh
 echo >&2 "Installing or upgrading shaarli in $(pwd) - copying now..."
-find -maxdepth 1 ! -regex '^\./data.*$' ! -regex '^\.$' -exec rm -rf {} +
-tar cf - --one-file-system -C /usr/src/shaarli . | tar xf -
-chown -R nginx:nginx .
+find -maxdepth 1 ! -regex '^\./data.*$' ! -regex '^\.$' -exec rm -rvf {} +
+tar cvf - --one-file-system -C /usr/src/shaarli . | tar xvf -
+chown -Rfv nginx:nginx .
 echo >&2 "Complete! Shaarli has been successfully installed / upgraded to $(pwd)"
 
 # Exec main command
